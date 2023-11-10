1e398043089a973b5a55486ece979cbf24637992
Wasserflasche <50297251+Wasserflasche@users.noreply.github.com>
2023-11-10 10:46:43 +0100

    Update fiestaquest.py

--- fiestaquest.py
+++ fiestaquest.py
@@ -1,140 +1,230 @@
 import ctypes
 import cv2
-import os
-import time
-import pytesseract
 import numpy as np
+import os
+import pyautogui
+import time
 from ctypes import windll
+from ctypes import wintypes
 from multiprocessing import Process
 from pynput import keyboard
-from typing import Dict, List, Tuple
-import pyautogui
+from typing import Tuple
 
-pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 currentPath = os.path.dirname(os.path.realpath(__file__))
 resoulution = (1920, 1080)
+
 #################################################### Help Functions ####################################################### 
-        
-def ReadFromFile(fileName: str):
+
+def CheckPixelsAbove(image: np.ndarray, max_loc: Tuple[int, int], offset: int = 20) -> bool:
+    rows = image[max_loc[1]-1 - offset:max_loc[1]+ 2 - offset, :]
+    mask = cv2.inRange(cv2.cvtColor(rows, cv2.COLOR_RGB2BGR), (220, 220, 0), (255, 255, 5))
+    return np.any(mask)
+
+def OpenQuestWindow() -> Tuple[int, int]:
+    baseCoords: Tuple[int, int] = (0,0)
+    while baseCoords == (0,0):
+        PressKey(l)
+        baseCoords = GetCoordsFromDetection(cv2.imread(currentPath + "\\fiestaQuest.png"))
+    return baseCoords
+
+def ReadFromFile(fileName: str) -> str:
     with open(fileName) as file:
         return file.read()
 
 def WriteToFile(fileName: str, input: str):
     with open(fileName, "w") as file:
-        file.write(str(input))  
+        file.write(str(input))    
    
-def GetBoxes(lower: Tuple [int, int, int], upper: Tuple [int, int, int]):
-    return pytesseract.image_to_data(cv2.inRange(Screenshot(), lower, upper), output_type='dict')   
-   
-def GetCoordsFromBoxes(boxes: Dict[str, List[int]], index: int):
-    x, y, w, h = (boxes['left'][index],boxes['top'][index],boxes['width'][index],boxes['height'][index],)
-    return (x + w // 2, y + h // 2)   
-   
-def GetCoordsFromDetection(image: np.ndarray):
+def GetCoordsFromDetection(image: np.ndarray) -> Tuple[int, int]:
     result = cv2.matchTemplate(Screenshot(), image, cv2.TM_CCOEFF_NORMED) 
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-    x, y = max_loc
-    return(x,y)   
- 
-def Screenshot():
-    return pyautogui.screenshot().convert("RGB")
+    if max_val > 0.9:
+        return (max_loc)
+    else:
+        return (0, 0)
     
-############################################################################################### Mouse and Keyboard ##############################################################################################################    
+def GetCoordsFromDetectionChat(image: np.ndarray) -> Tuple[int, int]:
+    screenshot = Screenshot()
+    result = cv2.matchTemplate(screenshot, image, cv2.TM_CCOEFF_NORMED) 
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    if max_val > 0.9:
+        x, y = max_loc
+        if CheckPixelsAbove(screenshot, max_loc):
+            y -= 20
+        return (x, y)
+    else:
+        return (0, 0)  
+
+def Screenshot() -> np.ndarray:
+    return cv2.cvtColor(np.array(pyautogui.screenshot().convert('RGB')), cv2.COLOR_RGB2BGR)
    
-MOUSEEVENTF_RIGHTDOWN = 0x0008
-MOUSEEVENTF_RIGHTUP = 0x0010
+############################################################################################### Mouse and Keyboard ##############################################################################################################    
+      
+MOUSEEVENTF_RIGHTDOWN = 0x0008 
+MOUSEEVENTF_RIGHTUP = 0x0010 
 MOUSEEVENTF_LEFTDOWN = 0x0002
 MOUSEEVENTF_LEFTUP = 0x0004
 MOUSEEVENTF_MOVE = 0x0001
-MOUSEEVENTF_ABSOLUTE = 0x8000   
+MOUSEEVENTF_ABSOLUTE = 0x8000
+one = 0x31
+two = 0x32
+three = 0x33
+four = 0x34
+five = 0x35
+six = 0x36
+seven = 0x37
+e = 0x45
+k = 0x4B
+l = 0x4C
+q = 0x51
+s = 0x53
+skills = [three, four, five, six, seven]
+user32 = ctypes.WinDLL('user32', use_last_error=True)
+
+INPUT_KEYBOARD = 1
+INPUT_HARDWARE = 2
+
+KEYEVENTF_EXTENDEDKEY = 0x0001
+KEYEVENTF_KEYUP       = 0x0002
+KEYEVENTF_UNICODE     = 0x0004
+KEYEVENTF_SCANCODE    = 0x0008
+
+MAPVK_VK_TO_VSC = 0
+
+wintypes.ULONG_PTR = wintypes.WPARAM
+
+class MOUSEINPUT(ctypes.Structure):
+    _fields_ = (("dx",          wintypes.LONG),
+                ("dy",          wintypes.LONG),
+                ("mouseData",   wintypes.DWORD),
+                ("dwFlags",     wintypes.DWORD),
+                ("time",        wintypes.DWORD),
+                ("dwExtraInfo", wintypes.ULONG_PTR))
+
+class KEYBDINPUT(ctypes.Structure):
+    _fields_ = (("wVk",         wintypes.WORD),
+                ("wScan",       wintypes.WORD),
+                ("dwFlags",     wintypes.DWORD),
+                ("time",        wintypes.DWORD),
+                ("dwExtraInfo", wintypes.ULONG_PTR))
+
+    def __init__(self, *args, **kwds):
+        super(KEYBDINPUT, self).__init__(*args, **kwds)
+        if not self.dwFlags & KEYEVENTF_UNICODE:
+            self.wScan = user32.MapVirtualKeyExW(self.wVk, MAPVK_VK_TO_VSC, 0)
+
+class HARDWAREINPUT(ctypes.Structure):
+    _fields_ = (("uMsg",    wintypes.DWORD), ("wParamL", wintypes.WORD), ("wParamH", wintypes.WORD))
+
+class INPUT(ctypes.Structure):
+    class _INPUT(ctypes.Union):
+        _fields_ = (("ki", KEYBDINPUT), ("mi", MOUSEINPUT), ("hi", HARDWAREINPUT))
+    _anonymous_ = ("_input",)
+    _fields_ = (("type",   wintypes.DWORD),("_input", _INPUT))
+
+LPINPUT = ctypes.POINTER(INPUT)
+
+def _check_count(result, func, args):
+    if result == 0:
+        raise ctypes.WinError(ctypes.get_last_error())
+    return args
+
+user32.SendInput.errcheck = _check_count
+user32.SendInput.argtypes = (wintypes.UINT,
+                             LPINPUT,       
+                             ctypes.c_int) 
   
-def Click(position, count:int = 1):
-    MoveMouseTo(position) 
-    for i in range(count):        
-        MouseClick()   
-  
-def MouseClick():
-    ctypes.windll.user32.mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-    time.sleep(0.1)
-    ctypes.windll.user32.mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+def MouseClick(position: Tuple[int, int], clicks: int = 1):
+    MoveMouseTo(position)
+    for _ in range(clicks):
+        ctypes.windll.user32.mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        InputSleep()
+        ctypes.windll.user32.mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+        InputSleep()
    
 def MoveMouseTo(position: Tuple[int, int]):
     x_normalized = int(65535 * (position[0] / resoulution[0]))
     y_normalized = int(65535 * (position[1] / resoulution[1]))
     ctypes.windll.user32.mouse_event(MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE, x_normalized, y_normalized, 0, 0)   
-    time.sleep(0.05)
+    InputSleep()
    
 def MouseRightDown():
     ctypes.windll.user32.mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
-    time.sleep(0.05)
+    InputSleep()
     
 def MouseRightUp():   
     ctypes.windll.user32.mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0)
-    time.sleep(0.05)
+    InputSleep()
    
-def MouseMove(count:int):
-    for i in range(count):
-        windll.user32.mouse_event(1, 100, 0, 0, 0)
-        time.sleep(0.05)  
-  
-Keyboard: keyboard.Controller = keyboard.Controller()    
+def MouseMove():
+    windll.user32.mouse_event(1, 100, 0, 0, 0)
+    InputSleep()
+    windll.user32.mouse_event(1, 100, 0, 0, 0)
+    InputSleep()
+    windll.user32.mouse_event(1, 100, 0, 0, 0)
    
-def PressKey(key: str):
-    Keyboard.press(key) 
+def KeyDown(hexKeyCode):
+    x = INPUT(type=INPUT_KEYBOARD,
+              ki=KEYBDINPUT(wVk=hexKeyCode))
+    user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
+    time.sleep(0.035)
+   
+def PressKey(hexKeyCode):
+    KeyDown(hexKeyCode)
+    ReleaseKey(hexKeyCode)
+
+def ReleaseKey(hexKeyCode):
+    x = INPUT(type=INPUT_KEYBOARD,
+              ki=KEYBDINPUT(wVk=hexKeyCode,
+                            dwFlags=KEYEVENTF_KEYUP))
+    user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
+    time.sleep(0.035)
+
+def InputSleep():
+    time.sleep(0.02)    
 
 def On_press(key: str):
-    if any([key in z for z in [{keyboard.Key.shift, keyboard.KeyCode(char='+')}, {keyboard.Key.shift, keyboard.KeyCode(char='Ö')}]]):
+    if any([key in z for z in [{keyboard.KeyCode(char='+')}]]):
         if ReadFromFile(currentPath + "\\stop.txt") == "0":
             WriteToFile(currentPath + "\\stop.txt", "1")
         else:
             WriteToFile(currentPath + "\\stop.txt", "0") 
-    if any([key in z for z in [{keyboard.Key.shift, keyboard.KeyCode(char='ä')}, {keyboard.Key.shift, keyboard.KeyCode(char='Ä')}]]): 
+    if any([key in z for z in [{keyboard.Key.shift}]]): 
         WriteToFile(currentPath + "\\stop.txt", "2")
-        
+
 ############################################################################################### Main Functions #####################################################################################################
  
 def AcceptQuests():
-    DetectQuest()
+    baseCoords:Tuple[int, int] = OpenQuestWindow()
+    coords:Tuple[int, int] = (baseCoords[0] + 200, baseCoords[1] + 60)
+    MouseClick(coords)
+    MouseClick((coords[0], coords[1] + 45))
+    MouseClick((baseCoords[0] + 75, baseCoords[1] + 540))
+    ClickTrough()
+
+def ClickTrough():
+    image: np.ndarray = cv2.imread(currentPath + "\\fiestaQuest2.png")
     while True:
         time.sleep(0.1)
-        position = DetectPosition()
+        position: Tuple[int, int] = GetCoordsFromDetectionChat(image)
         if position != (0,0):
-            Click(position)
+            MouseClick(position)
         else:
-            AcceptQuest()
+            time.sleep(0.1)
             break
- 
-def AcceptQuest():
-    boxes = GetBoxes((246, 255, 0), (255, 255, 5))
-    for i in range(len(boxes['text'])):
-        if boxes['text'][i].strip().lower() != "" and boxes['text'][i].strip().lower() != " ":
-            Click(GetCoordsFromBoxes(boxes, i))
-            break
- 
-def DetectPosition():
-    boxes = GetBoxes((246, 255, 0), (255, 255, 5))
-    for i in range(len(boxes['text'])):
-        if boxes['text'][i].strip().lower() == 'weiter' or boxes['text'][i].strip().lower() == 'quest':
-            return GetCoordsFromBoxes(boxes, i)
-    return (0,0) 
- 
-def DetectQuest():
-    PressKey("l")
-    baseCoords = GetCoordsFromDetection(cv2.imread(currentPath + "fiestaQuest.png"))
-    coords = (baseCoords[0] + 200, baseCoords[1] + 119)
-    Click(coords)
-    Click((coords[0], coords[1] + 45))
-    Click((baseCoords[0] + 75, baseCoords[1] + 540))
-     
-def DetectQuestFinish():
-    boxes = GetBoxes((120, 0, 0), (255, 30, 30))
-    for i in range(len(boxes['text'])):
-        if boxes['text'][i].strip().lower() == "belohnung":
-            Click(GetCoordsFromBoxes(boxes, i))
-            return True 
-    return False
 
-def FiestaBot():
+def DetectFinishedQuest(baseCoords: Tuple[int, int]) -> bool:
+    image: np.ndarray = Screenshot()
+    rows: np.ndarray = image[baseCoords[1] + 90 :baseCoords[1] + 260, baseCoords[0] + 383 : baseCoords[0] + 448]
+    mask: np.ndarray = cv2.inRange(cv2.cvtColor(rows, cv2.COLOR_RGB2BGR), (220,0,0), (230,13,3))
+    if np.any(mask):
+        y, x = np.where(mask)
+        MouseClick((baseCoords[0] + x[0] + 383, baseCoords[1] + y[0] + 90))
+        return True
+    else:
+        return False
+
+def FiestaQuestAccepter():
     WriteToFile(currentPath + "\\stop.txt", "1")
     while True:
         if ReadFromFile(currentPath + "\\stop.txt") == "0":
@@ -146,36 +236,28 @@ def FiestaBot():
 
 def FinishQuests():
     if FinishedQuest():
-        while True:
-            position = DetectPosition()
-            if position != (0,0):
-                Click(position)
-            else:
-                AcceptQuest()
-                break
+        ClickTrough()
     else:
         WriteToFile(currentPath + "\\stop.txt", "1")
 
-def FinishedQuest():
-    PressKey("l")
+def FinishedQuest() -> bool:
+    baseCoords: Tuple[int, int] = OpenQuestWindow()
     for _ in range(5):
-        if DetectQuestFinish():
-            coords = GetCoordsFromDetection(cv2.imread(currentPath + "fiestaQuest.png"))
-            Click((coords[0] + 70, coords[1] + 540))
+        if DetectFinishedQuest(baseCoords):
+            MouseClick((baseCoords[0] + 60, baseCoords[1] + 537))
             return True
         else:
-            ScrollQuest()     
+            ScrollQuest(baseCoords)     
     return False
 
-def ScrollQuest():
-    coords = GetCoordsFromDetection(cv2.imread(currentPath + "fiestaQuest.png"))
-    Click((coords[0] + 500, coords[1] + 235), 7)
+def ScrollQuest(baseCoords: Tuple[int, int]):
+    MouseClick((baseCoords[0] + 500, baseCoords[1] + 230), 7)
 
 ############################################################################################### Start ##############################################################################################################
        
 if __name__ == '__main__':    
     processListener = keyboard.Listener(on_press=On_press)
-    processBot = Process(target = FiestaBot)
+    processBot = Process(target = FiestaQuestAccepter)
     processBot.start()
     processListener.start()
     processBot.join()
